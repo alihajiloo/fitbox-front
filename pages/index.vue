9e3e5e4ffbeff8ce150c8cd4f23b5ff7f24b9a81
alihajiloo <alihajiloo.ir@gmail.com>
2022-04-23 01:25:29 +0430

    style(css): add Lalezar font & change description font

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
         width="400"
         height="400"
       />
-      <h2 class="text-center text-3xl text-white">دمبل: سامانه ورزشی هوشمند</h2>
+      <h2 class="text-center text-2xl md:text-3xl lalezar text-white">دمبل: سامانه ورزشی هوشمند</h2>
     </div>
   </div>
 </template>
