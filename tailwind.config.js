81c2d6d8e2b94329503be95ef5262b11fcdd7543
Ali Hajiloo <AliHajiloo.ir@gmail.com>
2022-05-27 17:50:17 +0430

    style(tailwind): add new primary & accent color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,13 @@ module.exports = {
     extend: {
       colors: {
         primary: '#7678ed',
-
+        'primary-800': '#3d348b',
+        'accent-2': '#f35b04',
         accent: '#da4167',
         'accent-700': '#d62f59',
         'accent-500': '#dc4a6e',
         warn: '#d90429',
-        'dark-gary': '#363537'
+        'dark-gary': '#363537',
       },
       boxShadow: {
         primary: '0 4px 14px 0 rgba(0, 118, 255, 0.39)',
