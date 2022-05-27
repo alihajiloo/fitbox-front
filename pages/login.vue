0e1d3e9ff10899c48f780500c374f9bbe4b3bce3
Ali Hajiloo <AliHajiloo.ir@gmail.com>
2022-05-27 17:51:34 +0430

    style(login): format code

--- pages/login.vue
+++ pages/login.vue
@@ -7,9 +7,7 @@
     <body class="h-full">
     ```
   -->
-  <div
-    class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
-  >
+  <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
     <div class="max-w-md w-full space-y-8">
       <div>
         <NuxtLink to="/">
@@ -19,16 +17,18 @@
             alt="fitbox | فیت‌باکس"
           />
         </NuxtLink>
-        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          ورود
-        </h2>
-        <h3 class="text-center mt2 text-gray-800">
-          با اکانت فیت‌باکس خود وارد شوید
-        </h3>
-        <p dir="rtl" class="mt-2 text-center text-sm text-gray-600">
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">ورود</h2>
+        <h3 class="text-center mt2 text-gray-800">با اکانت فیت‌باکس خود وارد شوید</h3>
+        <p
+          dir="rtl"
+          class="mt-2 text-center text-sm text-gray-600"
+        >
           یا
           {{ ' ' }}
-          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+          <a
+            href="#"
+            class="font-medium text-indigo-600 hover:text-indigo-500"
+          >
             <NuxtLink to="register">
               <TButton dir="rtl">حساب بسازید.</TButton>
             </NuxtLink>
@@ -36,10 +36,18 @@
         </p>
       </div>
       <form class="mt-8 space-y-6">
-        <input type="hidden" name="remember" value="true" />
+        <input
+          type="hidden"
+          name="remember"
+          value="true"
+        />
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
-            <label for="phone" class="sr-only">موبایل</label>
+            <label
+              for="phone"
+              class="sr-only"
+              >موبایل</label
+            >
             <input
               id="phone"
               name="phone"
@@ -52,7 +60,11 @@
             />
           </div>
           <div>
-            <label for="password" class="sr-only">کلمه عبور</label>
+            <label
+              for="password"
+              class="sr-only"
+              >کلمه عبور</label
+            >
             <input
               id="password"
               name="password"
@@ -66,7 +78,10 @@
           </div>
         </div>
 
-        <div dir="rtl" class="flex items-center justify-between">
+        <div
+          dir="rtl"
+          class="flex items-center justify-between"
+        >
           <div class="flex items-center">
             <input
               id="remember-me"
@@ -74,13 +89,19 @@
               type="checkbox"
               class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
             />
-            <label for="remember-me" class="mr-2 block text-sm text-gray-900">
+            <label
+              for="remember-me"
+              class="mr-2 block text-sm text-gray-900"
+            >
               مرا به خاطر بسپار
             </label>
           </div>
 
           <NuxtLink to="/forgetPassword">
-            <h3 href="#" class="text-sm text-indigo-600 hover:text-indigo-500">
+            <h3
+              href="#"
+              class="text-sm text-indigo-600 hover:text-indigo-500"
+            >
               کلمه عبور خود را فراموش کرده اید؟
             </h3>
           </NuxtLink>
@@ -90,7 +111,7 @@
           <button
             @click.prevent="login(username, password)"
             type="submit"
-            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-primary hover:bg-primary-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-accent-2 hover:bg-primary-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
           >
             <span class="absolute left-0 inset-y-0 flex items-center pl-3">
               <LockClosedIcon
@@ -100,7 +121,12 @@
             </span>
             ورود
           </button>
-          <h3 dir="rtl" class="mt-2 text-sm text-center text-pink-800">{{ error }}</h3>
+          <h3
+            dir="rtl"
+            class="mt-2 text-sm text-center text-pink-800"
+          >
+            {{ error }}
+          </h3>
         </div>
       </form>
     </div>
@@ -114,12 +140,12 @@ export default {
       username: '',
       password: '',
       error: '',
-    }
+    };
   },
   methods: {
     async login(username, password) {
       // alert(username+":"+password)
-      let error2 = ''
+      let error2 = '';
       await this.$axios
         .post('/auth/login', {
           username: this.username,
@@ -127,15 +153,15 @@ export default {
         })
         .then(function (response) {
           // after login completes
-          location.replace('/dashboard')
+          location.replace('/dashboard');
         })
         .catch(function (error) {
           if (error.response.status === 401) {
-            error2 = 'موبایل یا کلمه عبور اشتباه است!'
+            error2 = 'موبایل یا کلمه عبور اشتباه است!';
           }
-        })
-      this.error = error2
+        });
+      this.error = error2;
     },
   },
-}
+};
 </script>
