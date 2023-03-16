1102f18fef6bd92038c806caa5cb980dd5adea12
Ali Hajiloo <alihajiloo.ir@gmail.com>
2023-03-16 20:02:25 +0330

    dummy

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "http://172.20.10.11:3000/api/v1/",
+    baseURL: "http://5.78.51.118:3000/api/v1/",
   },
 
   router: {
